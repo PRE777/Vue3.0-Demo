91e907537dfc530e74770797a59e18826916ac0e
李 伟 <1072139097@qq.com>
2020-11-20 10:11:39 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -6,9 +6,11 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     // publicPath: process.env.NODE_ENV === 'production' ? './' : './',
-    publicPath: '/',
+    publicPath: './',
     assetsDir: 'static',
-    outputDir: process.env.outputDir,
+    // outputDir: process.env.outputDir,
+    outputDir: "dist1",
+
     // 避免Eslint报错
     lintOnSave: false,
     devServer: {
